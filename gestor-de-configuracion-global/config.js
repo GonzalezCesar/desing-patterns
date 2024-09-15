8d5e486abe0de39d8307088ea61355ee139f8ca8
GonzalezCesar <gonzalez.abachecesar@gmail.com>
2024-09-14 21:31:00 -0400

    Patron Singleton changes

--- gestor-de-configuracion-global/config.js
+++ gestor-de-configuracion-global/config.js
@@ -1,32 +1,26 @@
 // config.js
 class Config {
     constructor() {
-      // Comprueba si una instancia ya existe
       if (Config.instance) {
         return Config.instance;
       }
       
-      // Si no existe, define las configuraciones iniciales
       this.settings = {
         theme: 'dark',
         language: 'en',
       };
   
-      // Guarda la instancia en la propiedad estática
       Config.instance = this;
     }
   
-    // Método para obtener una configuración
     getSetting(key) {
       return this.settings[key];
     }
   
-    // Método para actualizar una configuración
     setSetting(key, value) {
       this.settings[key] = value;
     }
   }
   
-  // Exporta la clase
   export default Config;
   
--- gestor-de-configuracion-global/main.js
+++ gestor-de-configuracion-global/main.js
@@ -1,13 +1,10 @@
 // main.js
 import Config from './config.js';
 
-// Crea una instancia de Config
 const appConfig = new Config();
 
-// Actualiza la configuración
 appConfig.setSetting('theme', 'light');
 appConfig.setSetting('language', 'es');
 
-// Muestra la configuración en la pantalla
 document.getElementById('theme').textContent = `Tema: ${appConfig.getSetting('theme')}`;
 document.getElementById('language').textContent = `Idioma: ${appConfig.getSetting('language')}`;
